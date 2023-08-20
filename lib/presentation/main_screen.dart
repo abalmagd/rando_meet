f18ed5ee0e5097ce77bece65dd3081ba6de5c7ae
abalmagd <me@abulmagd.dev>
2023-08-20 19:34:56 +0300

    setup connection methods

--- lib/presentation/main_screen.dart
+++ lib/presentation/main_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
 
@@ -12,7 +14,6 @@ class _MainScreenState extends State<MainScreen> {
   final _localVideoRenderer = RTCVideoRenderer();
   final _remoteVideoRenderer = RTCVideoRenderer();
 
-  final _sdpController = TextEditingController();
   bool _offer = false;
 
   RTCPeerConnection? _peerConnection;
@@ -23,16 +24,16 @@ class _MainScreenState extends State<MainScreen> {
     super.initState();
     _localVideoRenderer.initialize();
     getUserMedia();
+    _createPeerConnection();
   }
 
   @override
   void dispose() {
     _localVideoRenderer.dispose();
-    _sdpController.dispose();
     super.dispose();
   }
 
-  void getUserMedia() async {
+  Future<MediaStream> getUserMedia() async {
     final Map<String, dynamic> mediaConstraints = {
       'audio': true,
       'video': {'facingMode': 'user'},
@@ -42,12 +43,19 @@ class _MainScreenState extends State<MainScreen> {
         await navigator.mediaDevices.getUserMedia(mediaConstraints);
 
     _localVideoRenderer.srcObject = stream;
+
+    return stream;
   }
 
-  void _createPeerConnection() {
+  Future<RTCPeerConnection> _createPeerConnection() async {
     Map<String, dynamic> configuration = {
-      "iceServers": [
-        {"url": "stun:stun.l.google.com:19302"},
+      'iceServers': [
+        /*{'url': 'stun:stun.l.google.com:19302'},*/
+        {
+          'url': 'turn:turn.i2-host.com:3478',
+          'username': 'saritest',
+          'credential': 'test546321',
+        }
       ]
     };
 
@@ -58,6 +66,77 @@ class _MainScreenState extends State<MainScreen> {
       },
       "optional": [],
     };
+
+    _localStream = await getUserMedia();
+
+    RTCPeerConnection pc =
+        await createPeerConnection(configuration, offerSdpConstraints);
+
+    if (_localStream == null) {
+      debugPrint('Local stream is null');
+    }
+
+    pc.addStream(_localStream!);
+
+    pc.onIceCandidate = (e) {
+      if (e.candidate != null) {
+        debugPrint(json.encode({
+          'candidate': e.candidate,
+          'sdpMid': e.sdpMid,
+          'sdpMlineIndex': e.sdpMLineIndex
+        }));
+      }
+    };
+
+    pc.onIceConnectionState = (e) {
+      debugPrint(e.toString());
+    };
+
+    pc.onAddStream = (stream) {
+      debugPrint('addStream: ${stream.id}');
+      _remoteVideoRenderer.srcObject = stream;
+    };
+
+    return pc;
+  }
+
+  void _createOffer() async {
+    RTCSessionDescription description =
+        await _peerConnection!.createOffer({'offerToReceiveVideo': 1});
+    var session = description.sdp.toString();
+    debugPrint(json.encode(session));
+    _offer = true;
+
+    _peerConnection!.setLocalDescription(description);
+  }
+
+  void _createAnswer() async {
+    RTCSessionDescription description =
+        await _peerConnection!.createAnswer({'offerToReceiveVideo': 1});
+
+    var session = description.sdp.toString();
+    debugPrint(json.encode(session));
+
+    _peerConnection!.setLocalDescription(description);
+  }
+
+  void _setRemoteDescription() async {
+    String sdp = 'Test run';
+
+    RTCSessionDescription description =
+        RTCSessionDescription(sdp, _offer ? 'answer' : 'offer');
+    debugPrint(description.toMap());
+
+    await _peerConnection!.setRemoteDescription(description);
+  }
+
+  void _addCandidate() async {
+    String jsonString = 'Test Run';
+    dynamic session = await jsonDecode(jsonString);
+    debugPrint(session['candidate']);
+    dynamic candidate = RTCIceCandidate(
+        session['candidate'], session['sdpMid'], session['sdpMlineIndex']);
+    await _peerConnection!.addCandidate(candidate);
   }
 
   @override
@@ -71,7 +150,7 @@ class _MainScreenState extends State<MainScreen> {
             height: double.infinity,
             color: Colors.green.withOpacity(0.3),
             child: RTCVideoView(
-              _localVideoRenderer,
+              _remoteVideoRenderer,
               objectFit: RTCVideoViewObjectFit.RTCVideoViewObjectFitCover,
             ),
           ),
